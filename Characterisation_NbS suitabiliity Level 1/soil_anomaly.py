8c4eab4f253655aa3322b8ed39c90db225e45feb
AnnaSpin <52497361+AnnaSpin@users.noreply.github.com>
2025-11-12 14:27:46 +0100

    updated to save geotiff of soil moisture and soil anomalies

--- Characterisation_NbS suitabiliity Level 1/soil_anomaly.py
+++ Characterisation_NbS suitabiliity Level 1/soil_anomaly.py
@@ -30,10 +30,19 @@ import pandas as pd
 import os
 import numpy as np
 import rasterio
+import glob
+
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.dirname(current_dir)
+input_dir = os.path.join(parent_dir, "data", "input")
+out_dir = os.path.join(parent_dir, "data", "output")
+
+soil_moisture_dir = os.path.join(out_dir, "soil_moisture") #create output directory
+os.makedirs(soil_moisture_dir, exist_ok=True) #make sure output directory exists
+
+file_path = os.path.join(input_dir, 'soilw.mon.mean.v2.nc') #Downlod the file manually and place it here
 
 # 1. Load dataset without dask
-file_path = r"C:/Users/Downloads/soilw.mon.mean.v2.nc" # Update with your local path
-out_dir = r"C:/Users/Downloads"                     # Update with your desired output directory
 ds = xr.open_dataset(file_path)
 print(ds)
  
@@ -119,7 +128,52 @@ fig.colorbar(im, ax=axes[5, :2], shrink=0.4, pad=0.05, location='bottom',
 fig.suptitle('Mediterranean Soil Moisture Anomaly - 2024', fontsize=16)
 
 plt.show()
-#%%
+#%% To download the anomalies as GeoTIFFs
+
+# da = anomaly2024.rename({'lon': 'x', 'lat': 'y'})
+# # 2. Set spatial dimensions
+# da = da.rio.set_spatial_dims(x_dim='x', y_dim='y')
+# # 3. Assign CRS (WGS84)
+# da= da.rio.write_crs("EPSG:4326", inplace=True)  # set CRS (WGS84)
+
+# # 4. reduce dtype for smaller files
+# da = da.astype('float32')
+
+# # 5. write compressed multi-band GTiff (time->bands)
+# for i in range(da.sizes['time']):
+#     arr = da.isel(time=i)
+#     date_str = pd.to_datetime(arr.time.values).strftime("%Y-%m")
+#     out_file = os.path.join(out_dir, f"anomaly_{date_str}.tif")
+#     arr.rio.to_raster(out_file, driver="GTiff", compress="LZW", tiled=True)
+#     print("Wrote:", out_file)
+
+#%% If you want to save the data only for the Mediterranean region
+# 6. Clip to boundary 
+
+# Clip to bounding box: [min_lon, min_lat, max_lon, max_lat]
+da = soilw_2024.rename({'lon': 'x', 'lat': 'y'})
+da = da.rio.set_spatial_dims(x_dim='x', y_dim='y')
+da= da.rio.write_crs("EPSG:4326", inplace=True)  # set CRS (WGS84)
+da = da.astype('float32')
+da = da.rio.clip_box(minx=-10, miny=35, maxx=30, maxy=50)
+
+# Reduce dtype
+da = da.astype('float32')
+
+# Save each time slice as compressed GeoTIFF
+for i in range(da.sizes['time']):
+    arr = da.isel(time=i)
+    date_str = pd.to_datetime(arr.time.values).strftime("%Y-%m")
+    out_file = os.path.join(soil_moisture_dir, f"soil_moisture_MED_{date_str}.tif")
+    arr.rio.to_raster(out_file, driver="GTiff", compress="LZW", tiled=True)
+    print("Wrote:", out_file)
+
+# if you have a shapefile boundary, you can use:
+# import geopandas as gpd
+# boundary = gpd.read_file("path_to_your_shapefile.shp")
+# da = da.rio.clip(boundary.geometry, boundary.crs)
+
+# %% Save anomaly data for Mediterranean region only
 
 da = anomaly2024.rename({'lon': 'x', 'lat': 'y'})
 # 2. Set spatial dimensions
@@ -129,18 +183,6 @@ da= da.rio.write_crs("EPSG:4326", inplace=True)  # set CRS (WGS84)
 
 # 4. reduce dtype for smaller files
 da = da.astype('float32')
-
-# 5. write compressed multi-band GTiff (time->bands)
-for i in range(da.sizes['time']):
-    arr = da.isel(time=i)
-    date_str = pd.to_datetime(arr.time.values).strftime("%Y-%m")
-    out_file = os.path.join(out_dir, f"anomaly_{date_str}.tif")
-    arr.rio.to_raster(out_file, driver="GTiff", compress="LZW", tiled=True)
-    print("Wrote:", out_file)
-
-#%% If you want to save the data only for the Mediterranean region
-# 6. Clip to boundary 
-
 # Clip to bounding box: [min_lon, min_lat, max_lon, max_lat]
 da = da.rio.clip_box(minx=-10, miny=35, maxx=30, maxy=50)
 
@@ -151,13 +193,7 @@ da = da.astype('float32')
 for i in range(da.sizes['time']):
     arr = da.isel(time=i)
     date_str = pd.to_datetime(arr.time.values).strftime("%Y-%m")
-    out_file = os.path.join(out_dir, f"anomaly_MED_{date_str}.tif")
+    out_file = os.path.join(soil_moisture_dir, f"soil_moisture_anomalies_MED_{date_str}.tif")
     arr.rio.to_raster(out_file, driver="GTiff", compress="LZW", tiled=True)
     print("Wrote:", out_file)
-
-# if you have a shapefile boundary, you can use:
-# import geopandas as gpd
-# boundary = gpd.read_file("path_to_your_shapefile.shp")
-# da = da.rio.clip(boundary.geometry, boundary.crs)
-
 # %%
